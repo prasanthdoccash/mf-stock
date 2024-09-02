8063833a2f3bb662b0aa80d57089ed94e4d91a5a
prasanthdoccash <64533752+prasanthdoccash@users.noreply.github.com>
2024-09-02 15:43:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def refresh():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
